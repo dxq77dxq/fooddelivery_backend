79a6ed9edb37120d31cfb1792d0af5b0b1c78132
maggie <maggie@maggies-air.lan>
2021-09-27 09:55:33 -0400

    update

--- config/plugins.js
+++ config/plugins.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'aws-s3',
     providerOptions: {
-      accessKeyId: env('AWS_ACCESS_KEY'),
-      secretAccessKey: env('AWS_SECRET_KEY'),
-      region: env('AWS_REGION'),
+      accessKeyId: ${process.env.AWS_ACCESS_KEY},
+      secretAccessKey: ${process.env.AWS_SECRET_KEY},
+      region: ${process.env.AWS_REGION},
       params: {
-        Bucket: env('AWS_BUCKET'),
+        Bucket: ${process.env.AWS_BUCKET},
       },
     },
   },
