7d53667780b94dca0dd8e7c694d2d2b0a0a4c826
maggie <maggie@maggies-air.lan>
2021-09-27 07:18:47 -0400

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5998fabae49a2ece370d07a337c528bb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '47fcf31f-5d00-4185-a997-2c6c5ecb2f89'
+};
